006fc259fd3afeb86277ac99d6002d7587e087c4
name <some@email.com>
2022-07-11 20:53:42 +0200

    Included stages for event.

--- src/Event.py
+++ src/Event.py
@@ -1,15 +1,48 @@
 """Stores event information."""
 from datetime import datetime
+from typing import List
 
 from helper import parse_string
+from Person import Person
+
+
+class Event_stage:
+    """Stage of event."""
+
+    # pylint: disable=R0903
+    def __init__(self, stage):
+        if stage == "planning":
+            self.stage = "planning"
+        elif stage == "contacting":
+            self.stage = "contacting"
+        elif stage == "confirmed":
+            self.stage = "confirmed"
+        elif stage == "preparation":
+            self.stage = "preparation"
+        elif stage == "active":
+            self.stage = "active"
+        elif stage == "ended":
+            self.stage = "ended"
+        elif stage == "completed":
+            self.stage = "completed"
 
 
 class Event:
     """Information on event."""
 
     # pylint: disable=R0903
-    def __init__(self, name: str, location: str, date_and_time: datetime):
+    # pylint: disable=R0913
+    def __init__(
+        self,
+        date_and_time: datetime,
+        location: str,
+        name: str,
+        participants: List[Person],
+        stage: Event_stage,
+    ):
         # Person identification
         self.name: str = parse_string(name)
         self.location: str = parse_string(location)
         self.datetime: datetime = date_and_time
+        self.participants: List[Person] = participants
+        self.stage: Event_stage = stage
